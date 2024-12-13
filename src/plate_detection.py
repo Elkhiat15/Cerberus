8ba52a96df7ad4f3c29cc5a94e8b4e67f9283890
Mohammed Elkhiat <mohammed.khayyat02@eng-st.cu.edu.eg>
2024-12-14 00:16:33 +0200

    Added some functions to help in plate edge detection

--- src/plate_detection.py
+++ src/plate_detection.py
@@ -39,3 +39,50 @@ class LicensePlateDetector:
         
         return thresholded_image, ratio
 
+    def process_sobel_gradient(self, thresholded_image, ratio):
+        """Process Sobel gradient and apply morphological operations"""
+        # Sobel x-direction gradient
+        sobel_x = cv.Sobel(thresholded_image, cv.CV_64F, 1, 0, ksize=3)
+        sobel_x = np.absolute(sobel_x)
+        max_gradient_value = np.max(sobel_x)
+        sobel_x = 255 * ((sobel_x) / (max_gradient_value))
+        sobel_x = sobel_x.astype("uint8")
+        
+        # Conditional dilation
+        gradient_image = sobel_x
+        if ratio < 0.0042:
+            gradient_image = cv.dilate(sobel_x, None, iterations=2)
+        
+        # Closing operation
+        closing_kernel = cv.getStructuringElement(cv.MORPH_RECT, (7,7))
+        closed_image = cv.morphologyEx(gradient_image, cv.MORPH_CLOSE, closing_kernel)
+        
+        return closed_image
+
+    def refine_image(self, closed_image, ratio):
+        """Apply series of morphological operations to refine the image"""
+        # Erosion and dilation cycles
+        eroded_1 = cv.erode(closed_image, None, iterations=2)
+        dilated_1 = cv.dilate(eroded_1, None, iterations=3)
+        
+        eroded_2 = cv.erode(dilated_1, None, iterations=2)
+        dilated_2 = cv.dilate(eroded_2, None, iterations=3)
+        
+        # Thresholding
+        dilated_2[dilated_2 < 140] = 0
+        dilated_2[dilated_2 >= 140] = 255
+        
+        # Additional erosion based on ratio
+        eroded_3 = cv.erode(dilated_2, None, iterations=2)
+        if ratio >= 0.01:
+            erosion_kernel = cv.getStructuringElement(cv.MORPH_RECT, (3,3))
+            eroded_3 = cv.erode(eroded_3, erosion_kernel, iterations=2)
+        
+        # Final morphological operations
+        final_dilated_image = cv.dilate(eroded_3, None, iterations=8)
+        vertical_se = cv.getStructuringElement(cv.MORPH_RECT, (5,1))
+        final_img = cv.dilate(final_dilated_image, vertical_se, iterations=3)
+        
+        return final_img
+
+
