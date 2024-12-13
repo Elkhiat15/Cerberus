21a8ea0dc746f65c6946aad518f1b742af4c7018
Mohammed Elkhiat <mohammed.khayyat02@eng-st.cu.edu.eg>
2024-12-14 00:43:43 +0200

    Added the main function that combine all together

---
+++ src/pipeline.py
@@ -0,0 +1,40 @@
+import numpy as np
+import pandas as pd
+from character_extractor import *
+from plate_enhancer import *
+from license_plate_detector import *
+from joblib import load
+from skimage.feature import hog
+
+def process_image(image):
+    
+    car_image = np.array(image)
+    lpd = LicensePlateDetector()
+    enhancer = LicensePlateEnhancer()
+    extractor = CharExtractor()
+
+    plate = lpd.detect(car_image)
+    enhanced_plate = enhancer.enhance_plate(plate)
+    car_plate , flag, car_letters = extractor.extractChars(enhanced_plate)
+    
+    car_plate = np.array(car_plate)
+    result = []
+    if flag == 1:
+        model = load("trained_model.pkl", mmap_mode="r")
+        data = []
+        car_letters = sorted(car_letters ,key=lambda x: x[1], reverse = True)
+        for i in car_letters:
+            car_image = cv.resize(i[0],(32,64))
+            gray = cv.cvtColor(car_image, cv.COLOR_BGR2GRAY)
+            describtor= hog(gray,orientations=9,pixels_per_cell=(8,8), cells_per_block=(1, 1))
+            data.append((describtor).flatten())
+        df_test = pd.DataFrame(data)
+        df_test = df_test.dropna(axis=1)
+        result = model.predict(df_test)
+        print(result)
+        flag = 0
+        car_letters = []
+    else: 
+        result = []
+    
+    return [car_plate,result]
